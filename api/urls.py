40ef683b6294d77a74e2f875bc92a238c00eca5d
Akshay78980 <p.akshaysugathan@gmail.com>
2024-11-30 20:10:34 +0530

    Adjusted the url configurations

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet
+from users.views import UserProfileViewSet
+from social_media.views import PostViewSet
 
 router = DefaultRouter()
 router.register('profiles', UserProfileViewSet)
+router.register('posts', PostViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('users.urls')),
-    path("", include('social_media.urls')),
+    path("api/", include('api.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
